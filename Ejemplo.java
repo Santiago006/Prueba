b0841eb5183431a8c4e1d4415cfc333d5947ac8e
Santiago006 <101900493+Santiago006@users.noreply.github.com>
2022-03-29 16:41:47 +0200

    Add files via upload

---
+++ Ejemplo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Tarde
+ */
+public class Ejemplo {
+    public static void main(String[] args) {
+    System.out.print("Hola Santiago");
+    System.out.print("Esto es nuevo");
+            }
+}
